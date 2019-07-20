8b107eae628a3a372b4aa238bf2a802341058289
shuimu <shuimu0579@gmail.com>
2019-07-20 18:12:06 +0800

    modified webpack

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -8,13 +8,13 @@ describe('webpack.base.js test case', () => {
     assert.equal(
       baseConfig.entry.index.indexOf(
         'builder-webpack/test/smoke/template/src/index/index.js'
-      ),
+      ) > -1,
       true
     );
     assert.equal(
       baseConfig.entry.search.indexOf(
         'builder-webpack/test/smoke/template/src/search/index.js'
-      ),
+      ) > -1,
       true
     );
   });
