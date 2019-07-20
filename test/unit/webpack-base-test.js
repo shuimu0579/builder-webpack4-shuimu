3d005a388e8c4cef670a8d00c8d84f77bb87fd8e
shuimu <shuimu0579@gmail.com>
2019-07-20 17:55:54 +0800

    modified webpack-base-test 文件

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -1,18 +1,17 @@
 // 引入断言库 assert
-// const assert = require('assert');
+const assert = require('assert');
 
 describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base.js');
 
   it('entry', () => {
-    console.log(baseConfig);
-    // assert.equal(
-    //   baseConfig.entry.index,
-    //   '/Users/apple/builder-webpack/test/smoke/template/src/index/index.js'
-    // );
-    // assert.equal(
-    //   baseConfig.entry.search,
-    //   '/Users/apple/builder-webpack/test/smoke/template/src/search/index.js'
-    // );
+    assert.equal(
+      baseConfig.entry.index,
+      '/Users/apple/builder-webpack/test/smoke/template/src/index/index.js'
+    );
+    assert.equal(
+      baseConfig.entry.search,
+      '/Users/apple/builder-webpack/test/smoke/template/src/search/index.js'
+    );
   });
 });
