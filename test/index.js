5d19af4c791f2a717f88949f87253d976cbceef3
shuimu <shuimu0579@gmail.com>
2019-07-21 10:33:07 +0800

    modified the code

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@
 // 单元测试需要用到 测试框架Mocha和 断言库assert
 const path = require('path');
 
-process.chdir(path.join(__dirname, './smoke/template'));
+process.chdir(path.join(__dirname, 'smoke/template'));
 
 describe('builder-webpack test case', () => {
   require('./unit/webpack-base-test.js');
