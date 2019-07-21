abcdc259a4c46a2299baf9862f5d3f1ae8e17bdb
shuimu <shuimu0579@gmail.com>
2019-07-21 11:12:41 +0800

    修改 path

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -7,13 +7,13 @@ describe('webpack.base.js test case', () => {
   it('entry', () => {
     assert.equal(
       baseConfig.entry.index.indexOf(
-        'builder-webpack-shuimu/test/smoke/template/src/index/index.js'
+        '/test/smoke/template/src/index/index.js'
       ) > -1,
       true
     );
     assert.equal(
       baseConfig.entry.search.indexOf(
-        'builder-webpack-shuimu/test/smoke/template/src/search/index.js'
+        '/test/smoke/template/src/search/index.js'
       ) > -1,
       true
     );
